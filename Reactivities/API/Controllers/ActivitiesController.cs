3d91829dd503628890a2beca03614137e3f4a3c1
PeterBriesch <pbriesch@live.com>
2023-12-14 17:21:23 +1300

    Created api to create activity

--- Reactivities/API/Controllers/ActivitiesController.cs
+++ Reactivities/API/Controllers/ActivitiesController.cs
@@ -1,8 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -11,23 +9,25 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-        }
         
         [HttpGet] //api/activities
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
 
-            return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query()); 
         }
 
         [HttpGet("{id}")] //api/activities/{id}
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return await Mediator.Send(new Details.Query{Id = id});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            await Mediator.Send(new Create.Command {Activity = activity});
+            return Ok();
         }
     }
 }
