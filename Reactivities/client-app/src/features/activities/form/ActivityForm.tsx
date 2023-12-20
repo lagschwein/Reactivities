16086a4559b7f9c4d016c98b9224629ba2b56e47
PeterBriesch <pbriesch@live.com>
2023-12-20 14:00:44 +1300

    End of section 8

--- Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
+++ Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,14 +1,22 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { Button, Form, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Activity } from '../../../app/models/activity';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
+import { v4 as uuid } from 'uuid'
 
 export default observer(function ActivityForm() {
 
   const { activityStore } = useStore();
-  const { selectedActivity, closeForm, createActivity, updateActivity, loading } = activityStore;
+  const { createActivity, updateActivity,
+    loading, loadActivity, loadingInitial } = activityStore;
 
-  const initialState = selectedActivity ?? {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [activity, setActivity] = useState<Activity>({
     id: '',
     title: '',
     category: '',
@@ -16,18 +24,27 @@ export default observer(function ActivityForm() {
     date: '',
     city: '',
     venue: ''
-  }
+  })
 
-  const [activity, setActivity] = useState(initialState);
+  useEffect(() => {
+    if (id) loadActivity(id).then(activity => setActivity(activity!))
+  }, [id, loadActivity])
 
   function handleSubmit() {
-    activity.id ? updateActivity(activity) : createActivity(activity)
+    if (!activity.id) {
+      activity.id = uuid();
+      createActivity(activity).then(() => navigate(`/activities/${activity.id}`))
+    } else {
+      updateActivity(activity).then(() => navigate(`/activities/${activity.id}`))
+    }
   }
 
   function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
     const { name, value } = event.target;
     setActivity({ ...activity, [name]: value })
   }
+
+  if (loadingInitial) return <LoadingComponent content='Loading activity...' />
 
   return (
     <Segment clearing>
@@ -38,8 +55,8 @@ export default observer(function ActivityForm() {
         <Form.Input type='date' placeholder='Date' value={activity.date} name='date' onChange={handleInputChange} />
         <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
         <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
-        <Button loading={loading} floated='right' positive type='submit' content='Submit' value={activity.title} name='title' onChange={handleInputChange} />
-        <Button onClick={closeForm} floated='right' type='button' content='Cancel' value={activity.title} name='title' onChange={handleInputChange} />
+        <Button loading={loading} floated='right' positive type='submit' content='Submit' />
+        <Button as={Link} to={`/activities/${activity.id}`} floated='right' type='button' content='Cancel' />
       </Form>
     </Segment>
   )
